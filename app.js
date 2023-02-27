bb4ff196fc3ffa30ca7bcb2cd50cff289daa4500
raflylesmana3003 <raflylesmana111@gmail.com>
2023-02-27 10:46:34 +0700

    feat: add redis layer

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const Redis = require("ioredis");
+const fetch = require("node-fetch");
+
+
+
+const app = express();
+const redis = new Redis('redis://redis:6379');
+
+app.get("/api/features/:sdk", async (req, res) => {
+  const cacheKey = "cache-key";
+
+  const value = await redis.get(cacheKey);
+  
+  if (value !== null) {
+    res.send(value);
+  } else {
+    // Retrieve data from Growthbook Proxy API
+    fetch("http://proxy:3300/api/features/"+req.params.sdk)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to retrieve data from Growthbook Proxy API");
+        }
+        return response.json();
+      })
+      .then(async (data) => {
+        await redis.set(cacheKey, JSON.stringify(data));
+        await redis.expire(cacheKey, 60 * 5);
+
+        res.send(data);
+      })
+      .catch(async (err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from Growthbook Proxy API");
+      });
+  }
+  
+});
+
+app.listen(3001, () => {
+  console.log("Cache layer listening on port 3001");
+});
+
